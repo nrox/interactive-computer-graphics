7753f444006ee12a428d96c1a0741636aff889e5
Nuno Rocha <nuno.rocha@de.abb.com>
2020-05-06 20:05:57 +0200

    test group

--- src/02-bubble-clusters/bubbles.js
+++ src/02-bubble-clusters/bubbles.js
@@ -1,10 +1,25 @@
 
+class Field {
+  constructor(item, radius){
+    this.item = item
+    this.radius = radius
+  }
+  valueAt({x, y}){
+    let center = this.item.getCenter()
+    let radius = this.radius
+    let distance = Math.sqrt(Math.pow(center.x - x, 2) + Math.pow(center.y - y, 2)) 
+    if (distance>=radius) return 0
+    return 1 - distance/radius
+  }
+}
+
 class Item {
   /**
    * @param {object} properties {x, y, width, height, cls} 
    */
   constructor(properties){
     this.prop = properties
+    this.field = new Field(this, properties.radius || properties.width)
   }
   setProp(name, value){
     this.prop[name] = value
@@ -47,16 +62,70 @@ class Item {
   }
 }
 
-class Field {
-  constructor(item, radius){
-    this.item = item
-    this.radius = radius
+
+class Group {
+  /**
+   * 
+   * @param {object} {delta, width, height} 
+   */
+  constructor(properties){
+    this.prop = properties
+    this.items = []
+    this.initField()
   }
-  valueAt({x, y}){
-    let center = this.item.getCenter()
-    let radius = this.radius
-    let distance = Math.sqrt(Math.pow(center.x - x, 2) + Math.pow(center.y - y, 2)) 
-    if (distance>=radius) return 0
-    return 1 - distance/radius
+  initField(){
+    const delta = this.prop.delta
+    const rows = Math.ceil(this.prop.height / delta)
+    const cols = Math.ceil(this.prop.width / delta)
+    let field = []
+    for (let xc = 0; xc < cols; xc++){
+      let row = []
+      for (let yr = 0; yr < rows; yr++){
+        row.push(0)
+      }
+      field.push(row)
+    }
+    this.field = field
+    this.rows = rows
+    this.cols = cols
+  }
+  resetField(){
+    let field = this.field
+    for (let xc = 0; xc < field.length; xc++){
+      for (let yr = 0; yr < field[xc].length; yr++){
+        field[xc][yr] = 0
+      }
+    }
+  }
+  increaseField(xc, yr, value){
+    this.field[xc][yr] += value
+  }
+  computeField(){
+    this.resetField()
+    let field = this.field
+    const delta = this.prop.delta 
+    for (let xc = 0; xc < field.length; xc++){
+      for (let yr = 0; yr < field[xc].length; yr++){
+        let position = {
+          x: xc * delta,
+          y: yr * delta
+        }
+        for (let i in this.items){
+          let item = this.items[i]
+          let value = item.field.valueAt(position)
+          this.increaseField(xc, yr, value)
+        }      
+      }
+    }
+    return this.field
+  }
+  add(item){
+    this.items.push(item)
+  }
+  addItems(items){
+    for (let i in items) this.add(items[i])
+  }
+  showField(draw){
+
   }
 }
--- src/02-bubble-clusters/tests/unit-tests.js
+++ src/02-bubble-clusters/tests/unit-tests.js
@@ -2,11 +2,23 @@
 
 describe("bubbles.js", () => {
 
+    let DRAW
+    let ITEMS
+
+    beforeAll(()=>{
+        DRAW = SVG('#svg')
+        ITEMS = createItems(DRAW)
+    })
+
     function createItems(draw){
-        let item = new Item({x: 0, y: 0, width: 30, height: 30, cls: 'item'})
+        let item = new Item({x: 0, y: 0, width: 30, height: 30, cls: 'item', radius: 25})
         let item1 = item.clone().create(draw).move(10, 20).setColor('#345')
         let item2 = item.clone().create(draw).move(30, 90).setColor('#891')
         return [item1, item2]
+    }
+
+    function testField(field, testFunction){
+        return field.map(row=>row.map(testFunction).reduce((a,b)=>a&&b)).reduce((a,b)=>a&&b)
     }
 
     it("...", () => {
@@ -14,9 +26,8 @@ describe("bubbles.js", () => {
     })
 
     it("Field", () => {
-        const draw = SVG('#svg')
-        const items = createItems(draw)
-        const item = items[0]
+        
+        const item = ITEMS[0]
         const radius = 25
         const field = new Field(item, radius)
 
@@ -33,6 +44,30 @@ describe("bubbles.js", () => {
         let farAway = item.getCenter()
         farAway.x += 1.5*radius
         expect(field.valueAt(farAway)).toBe(0)
+    })
+
+    it("Group", () => {
+        const prop = {delta: 20, width: 200, height: 200}
+        const group = new Group(prop)
+        group.addItems(ITEMS)
+        expect(group.items.length).toBe(ITEMS.length)
+
+        let all0 = testField(group.field, value => value == 0)
+        let numeric = testField(group.field, value => !isNaN(value))
+        expect(numeric).toBe(true)
+        expect(all0).toBe(true)
+        
+        group.computeField()
+        all0 = testField(group.field, value => value == 0)
+        numeric = testField(group.field, value => !isNaN(value))
+        expect(numeric).toBe(true)
+        expect(all0).toBe(false)
+
+        group.resetField()
+        all0 = testField(group.field, value => value == 0)
+        numeric = testField(group.field, value => !isNaN(value))
+        expect(numeric).toBe(true)
+        expect(all0).toBe(true)
     })
 
 })
